6c9e88c0baa15661150a2339354663c0976fb2ef
Zack Amin <ishaq.amin@co-operativebank.co.uk>
2020-09-09 12:43:00 +0100

    Linked lists methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,24 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    // Lists without a type <Interger> can hold a mix of information.
 
-        
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addFirst(1);
+        System.out.println(list.contains(25));
+        System.out.println(list.indexOf(20));
+        System.out.println(list.size());
+        System.out.println(list);
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
     }
 }
