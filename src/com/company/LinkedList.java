4f54d813627de748908e8b239f2c6b14a13a61c6
Zack Amin <ishaq.amin@co-operativebank.co.uk>
2020-09-10 10:51:22 +0100

    Updated commit

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -14,37 +14,49 @@ public class LinkedList {
     private Node first;
     private Node last;
     private int count;
+    private boolean containsTrue = true;
 
-        public void addLast(int item) {
-            var node = new Node(item);
-
-            if(first == null)
-                first = last = node;
-            else {
-                last.next = node;
-                last = node;
-            }
-
-        }
-
+//        public void addLast(int item) {
+//            var node = new Node(item);
+//
+//            if(first == null)
+//                first = last = node;
+//            else {
+//                last.next = node;
+//                last = node;
+//            }
+//        }
         public void addFirst(int item) {
             var node = new Node(item);
 
             if(first == null)
                 first = last = node;
             else {
+                node.next = first;
                 first = node;
             }
 
         }
 
-        public int indexOf(int item) {
+        public int indexOfNumber(int item) {
             for(int i = 0; i < count; i++) {
                 if (i == item) {
                     return i;
                 }
             }
+            return -1;
+        }
 
-            return item;
+        public boolean contains(int item) {
+            for(int i = 0; i < count; i++)
+                if(i == item) {
+                    return containsTrue;
+                }
+            return false;
+        }
+
+        public void removeFirst(){
+
+
         }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,11 +23,14 @@ public class Main {
 
        var list = new LinkedList();
 
+        list.addLast();
         list.addLast(10);
         list.addLast(20);
         list.addFirst(40);
         list.addFirst(99);
-        System.out.println(list.indexOf(20));
+        System.out.println(list.indexOf(45));
+        System.out.println(list.contains(99));
+        System.out.println(list.removeLast());
         System.out.println(list);
     }
 }
